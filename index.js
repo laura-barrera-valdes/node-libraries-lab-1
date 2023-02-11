8817420398118e3e71bd7aa9d3ea19f4a6361c0d
laura-barrera-valdes <laurabv99@gmail.com>
2023-02-10 21:52:28 -0500

    feat: new file js using numbers

---
+++ index.js
@@ -0,0 +1,12 @@
+const num = require('numbers');
+
+console.log(num.prime.simple(2));//true
+console.log(num.prime.simple(7));//true
+console.log(num.prime.simple(1));//false
+console.log(num.prime.simple(4));//false
+
+console.log(num.basic.gcd(4,20688780));//4
+console.log(num.basic.gcd(5,9232));//1
+console.log(num.basic.gcd(0,9232));//9232
+
+
